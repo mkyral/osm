431100a2b3c48836bc279713662d48258949e062
Marián Kyral <mkyral@email.cz>
2018-02-10 23:48:03 +0100

    ceska_posta: add data update scripts

--- import/ceska_posta/stats/dba/create_tables.sql
+++ import/ceska_posta/stats/dba/create_tables.sql
@@ -1,7 +1,7 @@
-create table post_boxes (
+create table osm_post_boxes (
     id               bigint,
-    latitude         numeric(20,4),
-    longitude        numeric(20,4),
+    latitude         numeric(20),
+    longitude        numeric(20),
     ref              character varying(255),
     operator         character varying(255),
     collection_times character varying(255),
@@ -17,14 +17,39 @@ create table cp_post_boxes (
     y numeric(20,4),
     lat numeric(20,10),
     lon numeric(20,10),
+    updated_lat numeric(20,10),
+    updated_lon numeric(20,10),
     address varchar(255),
     place varchar(255),
     suburb varchar(255),
     village varchar(255),
     district varchar(255),
     collection_times varchar(255),
+    state varchar(1),
     create_date timestamp,
     last_update timestamp,
+    source varchar(255)
+);
+
+comment on table cp_post_boxes is 'Czech post office data about post boxes';
+comment on column cp_post_boxes.state is 'Row state: A - active, D - deleted';
+
+create table cp_post_boxes_upload (
+    ref varchar(25)  NOT NULL,
+    psc numeric(10),
+    id numeric(10),
+    x numeric(20,4),
+    y numeric(20,4),
+    lat numeric(20,10),
+    lon numeric(20,10),
+    updated_lat numeric(20,10),
+    updated_lon numeric(20,10),
+    address varchar(255),
+    place varchar(255),
+    suburb varchar(255),
+    village varchar(255),
+    district varchar(255),
+    collection_times varchar(255),
     source varchar(255)
 );
 
@@ -33,9 +58,30 @@ create table cp_depos (
     name varchar(255)
 );
 
-grant select on cp_post_boxes to guest;
-grant select on post_boxes to guest;
-grant select on cp_depos to guest;
+create table cp_stats (
+    day date,
+    cp_timestamp   timestamp with time zone,
+    osm_timestamp  timestamp with time zone,
+    cp_total       bigint,
+    osm_total      bigint,
+    osm_linked     bigint,
+    osm_linked_pct number(6,2)
+);
 
+create table cp_data_state (
+    cp timestamp with time zone,
+    cp_source varchar(25),
+    osm timestamp with time zone
+);
+
+comment on table cp_data_state is 'Info about latest data refresh';
+comment on column cp_data_state.cp is 'Czech post boxes';
+comment on column cp_data_state.cp_source is 'Czech post source file';
+comment on column cp_data_state.osm is 'OSM import';
+
+grant select on cp_post_boxes to Public;
+grant select on osm_post_boxes to Public;
+grant select on cp_depos to Public;
+grant select on cp_data_state to Public;
 
 -- cat POST_SCHRANKY_201802.csv |cut -d ";" -f 1,2 |iconv -f cp1250 -t utf-8 |sort -u| sed "s/\([^;]*\);\(.*\)/insert into cp_depos values (\1, '\2');/" >cp_depos.sql
---
+++ import/ceska_posta/stats/dba/refresh_osm.sql
@@ -0,0 +1,50 @@
+
+\t
+-- Fill osm data from osm schema
+\echo '----------------------------------------------------------------------------'
+select 'Refreshing OSM data - '||to_char(current_timestamp, 'YYYYMMDD HH24:MI:SS');
+\echo '----------------------------------------------------------------------------'
+
+\timing on
+
+\echo
+\echo '* Clear table'
+truncate table osm_post_boxes;
+
+\echo
+\echo '* Insert actual post boxes'
+insert into osm_post_boxes (id, create_date)
+    select node_id, current_timestamp from current_node_tags where k = 'amenity' and v = 'post_box';
+
+\echo
+\echo '* Add LatLon data'
+update osm_post_boxes pb set (latitude, longitude) = (select latitude, longitude from current_nodes c where c.id = pb.id);
+
+\echo
+\echo '* Add Ref data'
+update osm_post_boxes pb set ref = (select v from current_node_tags c where c.node_id = pb.id and c.k = 'ref');
+
+\echo
+\echo '* Add collection_times data'
+update osm_post_boxes pb set collection_times = (select v from current_node_tags c where c.node_id = pb.id and c.k = 'collection_times');
+
+\echo
+\echo '* Add Operator data'
+update osm_post_boxes pb set operator = (select v from current_node_tags c where c.node_id = pb.id and c.k = 'operator');
+
+
+\echo
+\echo '* Update timestamp'
+update cp_data_state set osm = (select osm from import.datatimestamp LIMIT 1);
+
+
+\echo
+\echo '* Compute stats'
+
+
+\timing off
+
+\echo
+\echo '----------------------------------------------------------------------------'
+select 'Done - '||to_char(current_timestamp, 'YYYYMMDD HH24:MI:SS');
+\echo '----------------------------------------------------------------------------'
